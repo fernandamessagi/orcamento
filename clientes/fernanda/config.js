6552741c4474d211ebdcd24f462e6a3c7013e15a
fernandamessagi <messagi.salvatti@gmail.com>
2026-03-31 08:30:27 -0300

    Add files via upload

---
+++ clientes/fernanda/config.js
@@ -0,0 +1,8 @@
+window.CLIENT_CONFIG = {
+  installer: {
+    nome: "AC Arte Doce",
+    whats: "(11) 97239-1992",
+    pix: "11 97239 1992 / Catarina Barbara"
+  },
+  logo: "/climatizaorcamentos/clientes/fernanda/logo.png"
+};
