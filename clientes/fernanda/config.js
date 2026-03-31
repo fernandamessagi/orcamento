09268d3e2c3f2d9e3fc12e732548f4e273e4a04e
fernandamessagi <messagi.salvatti@gmail.com>
2026-03-31 08:32:40 -0300

    Update config.js

--- clientes/fernanda/config.js
+++ clientes/fernanda/config.js
@@ -1,8 +1,8 @@
 window.CLIENT_CONFIG = {
   installer: {
-    nome: "AC Arte Doce",
-    whats: "(11) 97239-1992",
-    pix: "11 97239 1992 / Catarina Barbara"
+    nome: "Komemora",
+    whats: "(14) 98825-0966",
+    pix: "14 98825 0966 / Fernanda Messagi"
   },
-  logo: "/climatizaorcamentos/clientes/fernanda/logo.png"
+  logo: "/orcamento/clientes/fernanda/logo.png"
 };
